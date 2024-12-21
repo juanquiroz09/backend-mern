7a5b9ba1f0976fb8c73ddc360268ffafb6a2ba81
[juanq] <[juan.quiroz@gruposaleta.com]>
2024-12-21 09:39:05 -0500

    se agrega url front

--- server/app.js
+++ server/app.js
@@ -1,18 +1,23 @@
 import express from "express";
 import morgan from "morgan";
 import fileUpload from "express-fileupload";
-import path from 'path'
+import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-
 import postRoutes from "./routes/posts.routes.js";
+import cors from 'cors';
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const FRONTEND_URL = process.env.FRONTEND_URL || "https://front-mern-context-crud-atjzbe2lh-juanquiroz09s-projects.vercel.app/"
 
+// Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Configuración básica para permitir todas las solicitudes
+app.use(cors());
 
 app.use(
   fileUpload({
@@ -21,9 +26,12 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, '../client/build')));
-
 // Routes
 app.use("/api", postRoutes);
+
+// Redirigir cualquier otra ruta al front-end
+app.get('*', (req, res) => {
+  res.redirect(`${FRONTEND_URL}${req.originalUrl}`);
+});
 
 export { app };
